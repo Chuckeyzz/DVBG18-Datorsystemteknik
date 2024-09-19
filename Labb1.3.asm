23f37609bff4761eac4a028a8295b5f5127df820
Chuckeyzz <69422746+Chuckeyzz@users.noreply.github.com>
2024-09-19 20:53:59 +0200

    Corrected version of assignment number three
    Psuedo code is better than no code.
    
    All packing and no Forza Horizon makes Chuckeyzz a dull boy.

--- Labb1.3.asm
+++ Labb1.3.asm
@@ -1,13 +1,48 @@
+
 .data
-	threshold: .word "204000"
-	high: .float ".5"
-	low: .float ".3"
+	thresh: .word 204000
+	prompt: .asciiz "\nVänligen mata in din brutto-inkomst : \n"
 .text
-
 	main:
+		li $v0 4		
+		la $a0 prompt
+		syscall	
 		
-
-
-
-
-	.include "print.s"
+		li $v0 5
+		syscall
+		
+		move $t0 $v0
+		lw $t1 thresh
+		ble $t0 $t1 lowroll	#kollar om t0 ligger under threshold
+		bgt $t0 $t1 highroll	#kollar om t0 ligger över threshold
+		
+		
+	lowroll:
+		mul $t3 $t0 30 
+		div $t4 $t3 100
+		sub $t5 $t0 $t4
+		move $a0 $t4
+		jal print
+		
+		li $v0 10
+		syscall
+		
+	highroll:
+		lw $t2 thresh		#laddar threshold
+		sub $t3 $t0 $t2		#subtrahera threshold
+		div $t4 $t3 2		#räkna ut högre skattesats på summa över threshold
+		
+		mul $t5 $t2 30		#kopia på lowroll-funktion
+		div $t6 $t5 100
+		sub $t9 $t0 $t6
+		#sub $t7 $t0 $t6
+		
+		sub $t8 $t9 $t4		#summera
+		move $a0 $t8
+		jal print
+	
+		li $v0 10
+		syscall
+	
+	
+.include "print.s"
